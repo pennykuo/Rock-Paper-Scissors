56f44c74e63fa2b17139c1a74311b8d57a2c1ff5
GETACAD\penny.kuo <penny.kuo@getac.cm.tw>
2024-02-26 13:10:24 +0800

    no message

--- java_script.js
+++ java_script.js
@@ -1,33 +1,32 @@
 // Your JavaScript goes here!
-while(1){
-let ans=parseInt(prompt("輸入1(剪刀),2(石頭),3(布)："))//輸入數字
-let a=Math.floor(Math.random()*3+1);//*(max - min + 1) + min)
-console.log(a);
-if(ans==1){
-    if(a==1){
-        console.log("平手")
-    }else if(a==2) {
-        console.log("You Lose! Rock beats Scissors")
-    }else if (a==3){
-        console.log("You Win! Sciccors beats Paper")
+
+    let ans = parseInt(prompt("輸入1(剪刀),2(石頭),3(布)："))//輸入數字
+    let rand = Math.floor(Math.random() * 3 + 1);//*(max - min + 1) + min)
+    console.log(ans);
+    if (ans == 1) {
+        if (rand == 1) {
+            console.log("平手")
+        } else if (rand == 2) {
+            console.log("You Lose! Rock beats Scissors")
+        } else if (rand == 3) {
+            console.log("You Win! Sciccors beats Paper")
+        }
+    } else if (ans == 2) {
+        if (rand == 1) {
+            console.log("You Win! Rock beats Paper")
+        } else if (rand == 2) {
+            console.log("平手")
+        } else if (rand == 3) {
+            console.log("You Lose! Papper beats Rock")
+        }
+    } else if (ans == 3) {
+        if (rand == 1) {
+            console.log("You Lose! Sciccors beats Papper")
+        } else if (rand == 2) {
+            console.log("You Win! Papper beats Rock")
+        } else if (rand == 3) {
+            console.log("平手")
+        }
+    } else {
+        console.log("輸入錯誤喔!");
     }
-}else if (ans==2){
-    if(a==1){
-        console.log("You Win! Rock beats Paper")
-    }else if(a==2) {
-        console.log("平手")
-    }else if (a==3){
-        console.log("You Lose! Papper beats Rock")
-    }
-}else if(ans==3){
-    if(a==1){
-        console.log("You Lose! Sciccors beats Papper")
-    }else if(a==2) {
-        console.log("You Win! Papper beats Rock")
-    }else if (a==3){
-        console.log("平手")
-    }
-}else{
-    console.log("輸入錯誤喔!");
-}
-}
